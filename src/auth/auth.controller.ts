759e319ad5b8797cb2c5e9eb9adb2bae801f9a9e
Nianoute <enzo.angot@gmail.com>
2023-07-24 08:51:50 +0200

    [fix]/dto signup change Signup by signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,24 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SigninAuthDto } from './dto/signin-auth.dto';
-import { SignupAuthDto } from './dto/Signup-auth.dto';
+import { SignupAuthDto } from './dto/signup-auth.dto';
 import { CreateTokenResetPasswordDto } from 'src/token-reset-password/dto/create-token-reset-password.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post("signup")
   @UseInterceptors(FilesInterceptor('file'))
   signup(
     @Body() signupAuthDto: SignupAuthDto,
     @UploadedFiles() file
-    ) {
+  ) {
     return this.authService.signup(signupAuthDto, file);
   }
-  
+
   @Post("signin")
   signin(@Body() signinAuthDto: SigninAuthDto) {
     return this.authService.signin(signinAuthDto);
